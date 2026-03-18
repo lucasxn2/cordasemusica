48874bcb63e2daa102b181c0a6a09e03f697e43b
Lucas <lxn562@gmail.com>
2026-03-18 16:03:54 -0300

    Criando as classes

---
+++ src/Classes/Main.java
@@ -0,0 +1,21 @@
+
+package Classes;
+
+/**
+ *
+ * @author lxn56
+ */
+public class Main {
+    public static void main(String[] args) {
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
